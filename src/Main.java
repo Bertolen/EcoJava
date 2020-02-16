2849ad23b208db75c363da190ec6f7d38ce30499
Bertolen <33809187+Bertolen@users.noreply.github.com>
2020-02-16 22:34:57 +0100

    Nouvelle classe abstraite Animal

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,21 @@ public class Main {
 		Pondeuse.pondreEtre(100, 100, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(120, 100, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(140, 100, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(160, 100, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(100, 120, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(120, 120, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(140, 120, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(160, 120, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(100, 140, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(120, 140, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(140, 140, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(160, 140, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(100, 160, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(120, 160, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(140, 160, Pondeuse.Espece.HERBE);
+		Pondeuse.pondreEtre(160, 160, Pondeuse.Espece.HERBE);
 		Pondeuse.pondreEtre(250, 250, Pondeuse.Espece.MOUTON);
+		Pondeuse.pondreEtre(150, 250, Pondeuse.Espece.MOUTON);
+		Pondeuse.pondreEtre(250, 150, Pondeuse.Espece.MOUTON);
 	}
 }
